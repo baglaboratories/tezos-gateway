11676a28d18862bbf6e0b643d663014a2ef1f30d
Andrew Kishino <AndrewKishino@gmail.com>
2019-08-03 08:23:34 -0700

    Add some additional ignore rules and eslint config

--- sample.js
+++ sample.js
@@ -16,24 +16,24 @@ const KEY = {
 const KEY2 = {
   sk: 'edsk46atr795NStaxX7mv7fNwJJ3edfpNdMeqMBBr8kKwbmdVpTiHQ',
   pkh: 'tz1iwPSJSQzcGqUfGR8GELxckpUNQfJYw3Wd',
-}
+};
 
 const tez = new Sotez('https://alphanet-node.tzscan.io', 'main', 'alpha');
 
 const depositOperation = {
-  "kind": "transaction",
-  "source": "tz1do3fDR8HB9cuUXu6m2zYH9qgudt1fqZQW",
-  "fee": "1000000",
-  "gas_limit": "800000",
-  "storage_limit": "60000",
-  "amount": "10000000",
-  "destination": "KT1JRpDEXMxGMaJgeJgEULkYYSynfLHG4Hzb",
-  "parameters": {
-    "prim": "Left",
-    "args": [
-      { "prim": "Unit" }
-    ]
-  }
+  kind: 'transaction',
+  source: 'tz1do3fDR8HB9cuUXu6m2zYH9qgudt1fqZQW',
+  fee: '1000000',
+  gas_limit: '800000',
+  storage_limit: '60000',
+  amount: '10000000',
+  destination: 'KT1JRpDEXMxGMaJgeJgEULkYYSynfLHG4Hzb',
+  parameters: {
+    prim: 'Left',
+    args: [
+      { prim: 'Unit' },
+    ],
+  },
 };
 
 const depositCredits = async () => {
@@ -47,34 +47,34 @@ const depositCredits = async () => {
 };
 
 const debitOperation = {
-  "kind": "transaction",
-  "source": "tz1do3fDR8HB9cuUXu6m2zYH9qgudt1fqZQW",
-  "fee": "1000000",
-  "gas_limit": "800000",
-  "storage_limit": "60000",
-  "amount": "0",
-  "destination": "KT1DMBFZ4f6PUHoG3Du5VXEW4XqKfBfoeWJs",
-  "parameters": {
-    "prim": "Right",
-    "args": [
+  kind: 'transaction',
+  source: 'tz1do3fDR8HB9cuUXu6m2zYH9qgudt1fqZQW',
+  fee: '1000000',
+  gas_limit: '800000',
+  storage_limit: '60000',
+  amount: '0',
+  destination: 'KT1DMBFZ4f6PUHoG3Du5VXEW4XqKfBfoeWJs',
+  parameters: {
+    prim: 'Right',
+    args: [
       [
         {
-          "prim": "Elt",
-          "args": [
-            { "string": "tz1do3fDR8HB9cuUXu6m2zYH9qgudt1fqZQW" },
-            { "int": "50" }
-          ]
+          prim: 'Elt',
+          args: [
+            { string: 'tz1do3fDR8HB9cuUXu6m2zYH9qgudt1fqZQW' },
+            { int: '50' },
+          ],
         },
         {
-          "prim": "Elt",
-          "args": [
-            { "string": "tz1iwPSJSQzcGqUfGR8GELxckpUNQfJYw3Wd" },
-            { "int": "50" }
-          ]
-        }
-      ]
-    ]
-  }
+          prim: 'Elt',
+          args: [
+            { string: 'tz1iwPSJSQzcGqUfGR8GELxckpUNQfJYw3Wd' },
+            { int: '50' },
+          ],
+        },
+      ],
+    ],
+  },
 };
 
 const debitCredits = async () => {
@@ -97,10 +97,10 @@ const debitCredits = async () => {
  * POST https://alphanet-node.tzscan.io/chains/main/blocks/head/helpers/parse/operations
  * Request:
  * {
- *   "operations": [
+ *   operations: [
  *     {
- *       "data": "0800002122d44d997e158c36c60649d198c4175dad425efa09d2a405f44e00f6f0b40201420eaa410ac21addf427211cddd6115cba385a94000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
- *       "branch": "BLpcXF8ADJbGuyUKNv7TypXRd5rqnoPn3PMqJLNBeRSr4VFeUuK"
+ *       data: '0800002122d44d997e158c36c60649d198c4175dad425efa09d2a405f44e00f6f0b40201420eaa410ac21addf427211cddd6115cba385a94000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+ *       branch: 'BLpcXF8ADJbGuyUKNv7TypXRd5rqnoPn3PMqJLNBeRSr4VFeUuK'
  *     }
  *   ]
  * }
@@ -108,20 +108,20 @@ const debitCredits = async () => {
  * Response:
  * [
  *   {
- *     "branch": "BLpcXF8ADJbGuyUKNv7TypXRd5rqnoPn3PMqJLNBeRSr4VFeUuK",
- *     "contents": [
+ *     branch: 'BLpcXF8ADJbGuyUKNv7TypXRd5rqnoPn3PMqJLNBeRSr4VFeUuK',
+ *     contents: [
  *       {
- *         "kind": "transaction",
- *         "source": "tz1NfEiS2uJsX43vowNjau5pdqg3Nvy8whvc",
- *         "fee": "1274",
- *         "counter": "86610",
- *         "gas_limit": "10100",
- *         "storage_limit": "0",
- *         "amount": "5060726",
- *         "destination": "KT1Ec3jNXyxyA54nezwcjGDRoutECJCQjpya"
+ *         kind: 'transaction',
+ *         source: 'tz1NfEiS2uJsX43vowNjau5pdqg3Nvy8whvc',
+ *         fee: '1274',
+ *         counter: '86610',
+ *         gas_limit: '10100',
+ *         storage_limit: '0',
+ *         amount: '5060726',
+ *         destination: 'KT1Ec3jNXyxyA54nezwcjGDRoutECJCQjpya'
  *       }
  *     ],
- *     "signature": "edsigtXomBKi5CTRf5cjATJWSyaRvhfYNHqSUGrn4SdbYRcGwQrUGjzEfQDTuqHhuA8b2d8NarZjz8TRf65WkpQmo423BtomS8Q"
+ *     signature: 'edsigtXomBKi5CTRf5cjATJWSyaRvhfYNHqSUGrn4SdbYRcGwQrUGjzEfQDTuqHhuA8b2d8NarZjz8TRf65WkpQmo423BtomS8Q'
  *   }
  * ]
  *
